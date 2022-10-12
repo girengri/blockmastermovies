9928387580d606a7832fc70b89a6ab523ad8c2c5
Giovany <giorengri.96@yahoo.com>
2022-10-11 20:42:04 -0500

    Cambio importacion bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/normalize.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import type { AppProps } from "next/app";
 
