9624be4027b969c82899edccb4c985bcbc6c1ea0
Giovany <giorengri.96@yahoo.com>
2022-10-11 21:41:39 -0500

    Implementacion de la palabra important en los estilos globales

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/normalize.css";
 import "../styles/globals.css";
+import "../styles/normalize.css";
 
 import type { AppProps } from "next/app";
 
